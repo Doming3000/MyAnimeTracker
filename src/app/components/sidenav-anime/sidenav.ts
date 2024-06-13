c5ead5766ed32e4a3a2f7f8dd43d3de629318ac2
Domingo <domingorv62@gmail.com>
2024-06-13 17:51:09 -0400

    .

--- src/app/components/sidenav-anime/sidenav.ts
+++ src/app/components/sidenav-anime/sidenav.ts
@@ -9,21 +9,25 @@ import { Component} from '@angular/core';
 export class SideNav {  
   // Abrir el menú desplegable
   isOpen: boolean = false;
-
+  
   openNav() {
     this.isOpen = true;
   }
-
+  
   closeNav() {
     this.isOpen = false;
   }
-
+  
   // Método para exportar archivo csv con el almacenamiento local
   exportData() {
-    // Obtener el contenido actual del almacenamiento local
     const storedData = localStorage.getItem("my_anime");
     
-    if (storedData !== null && storedData !== "[]") {
+    if (storedData === null || storedData === "[]") {
+      alert("No hay nada que descargar");
+      return;
+    }
+    
+    else {
       // Crear un blob con el contenido JSON
       const blob = new Blob([storedData], { type: "application/json" });
       
@@ -33,7 +37,7 @@ export class SideNav {
       // Crear un enlace <a> para descargar el archivo
       const a = document.createElement("a");
       a.href = url;
-      a.download = "Data.csv";
+      a.download = "data.csv";
       
       // Agregar el enlace al documento y simular un clic para iniciar la descarga
       document.body.appendChild(a);
@@ -41,8 +45,54 @@ export class SideNav {
       
       // Eliminar el enlace del documento
       document.body.removeChild(a);
-    } else {
-      alert("No hay nada que descargar");
     }
+  }
+  
+  // Método para importar datos desde un archivo CSV al almacenamiento local
+  importData(event: any) {
+    const file = event.target.files[0];
+    
+    // Verificar si se seleccionó un archivo
+    if (!file) {
+      alert("No se seleccionó ningún archivo");
+      return;
+    }
+    
+    else if (file.name !== "data.csv") {
+      alert("Este no parece ser el archivo correcto");
+      return;
+    }
+    
+    const reader = new FileReader();
+    
+    // Leer el contenido del archivo
+    reader.onload = (e: any) => {
+      const content = e.target.result;
+      try {
+        // Intentar parsear el contenido como JSON
+        const data = JSON.parse(content);
+        
+        // Verificar que el contenido sea un array (ajustar según tu estructura esperada)
+        if (!Array.isArray(data)) {
+          throw new Error("El contenido no tiene el formato correcto");
+        }
+        
+        // Almacenar los datos en el almacenamiento local
+        localStorage.setItem("my_anime", JSON.stringify(data));
+        
+        // Mostrar un mensaje de éxito
+        alert("Datos importados con éxito");
+        
+        // Recargar la ventana
+        window.location.reload();
+      } catch (error) {
+        // Mostrar un mensaje de error en caso de problemas con el parseo
+        alert("El archivo parece estar dañado y no es posible importarlo");
+        console.error(error);
+      }
+    };
+    
+    // Leer el archivo como texto
+    reader.readAsText(file);
   }
 }
